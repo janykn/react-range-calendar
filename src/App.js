617526b0c5ba0fd956fdee6681b27c5cc1614f8b
Janykn <janardhanykunta616@gmail.com>
2024-05-30 01:08:13 +0530

    Version 1.0.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DateRangePicker from "./DateRangePicker";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //(Jan(0),...., Dec(11)) January is 0th month and Dec is 11th month. it will be (YYYY, MM-1, DD) format for passsing dates as props
+  const minDate = new Date(2002, 6, 1); // January 1, 2022 
+  const maxDate = new Date(2023, 11, 31); // December 31, 2023
+  const initialStartDate = new Date(2023, 6, 14);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Date Range Picker</h1>
+      <DateRangePicker minDate={minDate} maxDate={maxDate} startDate={initialStartDate} theme="dark-theme"/>
     </div>
   );
-}
+};
 
 export default App;
